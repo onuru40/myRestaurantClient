abfdfb02d0c2ca0aa9c91967c98d517cae87513e
onuru40 <onur.unlu40@gmail.com>
2025-09-16 22:00:58 +0300

    t

--- src/app/admin-components/category/category.ts
+++ src/app/admin-components/category/category.ts
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './category.html',
   styleUrl: './category.css'
 })
+
 export class Category {
+
+title="Kategoriler Başlığı";
+
+category ={id:1,name:'Kategori 1'}
+
+categoryList = [
+  {id:5,name:'Kategori 1'},
+  {id:7,name:'Kategori 2'},
+  {id:9,name:'Kategori 3'},
+  {id:10,name:'Kategori 4'},
+  {id:11,name:'Kategori 5'}
+]
 
 }
