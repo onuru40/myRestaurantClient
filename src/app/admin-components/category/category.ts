b549c90cab2367607f25e6c333be7fd0a52671b2
onuru40 <onur.unlu40@gmail.com>
2025-09-16 21:10:22 +0300

    İlk component

---
+++ src/app/admin-components/category/category.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  standalone: false,
+  templateUrl: './category.html',
+  styleUrl: './category.css'
+})
+export class Category {
+
+}
--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Category } from './admin-components/category/category';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Category
   ],
   imports: [
     BrowserModule,
